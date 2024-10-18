57e8212a987a2ad3aefbba662c8125e523cdfb03
homee-jianzhi <116696497+HomeeJianzhi@users.noreply.github.com>
2024-10-18 17:12:27 +0800

    [update] : glitter version.

--- lowcode/Entry.js
+++ lowcode/Entry.js
@@ -55,7 +55,7 @@ export class Entry {
         }
         window.renderClock = (_a = window.renderClock) !== null && _a !== void 0 ? _a : clockF();
         console.log(`Entry-time:`, window.renderClock.stop());
-        glitter.share.editerVersion = "V_12.9.6";
+        glitter.share.editerVersion = "V_12.9.7";
         glitter.share.start = (new Date());
         const vm = {
             appConfig: [],
